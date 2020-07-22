aded84ecbcf897db7c1aab453672bffb9fcedb22
Edmond Ma <edjunma@gmail.com>
2020-07-21 22:43:29 -0400

    start on styling app

---
+++ src/Job.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
+
+export default function Job({ job }) {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<Card>
+			<Card.Body></Card.Body>
+		</Card>
+	);
+}
